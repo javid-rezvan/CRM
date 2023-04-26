0f89b6f5912a8cff525d4683d9b3bb660486726a
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-04-26 16:45:50 +0430

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     name=models.CharField(max_length=200,null=True)
     price=models.FloatField(null=True)
     category=models.CharField(max_length=200,null=True,choices=CATEGORY)
-    description=models.CharField(max_length=200,null=True)
+    description=models.CharField(max_length=200,null=True,blank=True)
     date_created=models.DateTimeField(auto_now_add=True,null=True)
     tags=models.ManyToManyField(Tag)
     
@@ -41,5 +41,7 @@ class Order(models.Model):
     product=models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
     date_created=models.DateTimeField(auto_now_add=True,null=True)
     status=models.CharField(max_length=200,null=True,choices=STATUS)
+    
+    
     
         
