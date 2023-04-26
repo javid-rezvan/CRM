7320497314d89396b5f5d0720b0dee30108a934d
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-04-26 16:33:32 +0430

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,11 @@ class Customer(models.Model):
     
     def __str__(self):
         return (self.name)
-
+class Tag(models.Model):
+      name=models.CharField(max_length=200,null=True)
+      
+      def __str__(self):
+        return self.name
 class Product(models.Model):
     CATEGORY = (
             ('Indoor','Indoor'),
@@ -19,6 +23,12 @@ class Product(models.Model):
     category=models.CharField(max_length=200,null=True,choices=CATEGORY)
     description=models.CharField(max_length=200,null=True)
     date_created=models.DateTimeField(auto_now_add=True,null=True)
+    tags=models.ManyToManyField(Tag)
+    
+    def __str__(self):
+        return self.name
+    
+
     
 class Order(models.Model):
     STATUS = (
@@ -27,8 +37,9 @@ class Order(models.Model):
             ('Deliverd','Deliverd'),
         )
     
-    #customer=
-    #product=
+    customer=models.ForeignKey(Customer,null=True,on_delete=models.SET_NULL)
+    product=models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
     date_created=models.DateTimeField(auto_now_add=True,null=True)
     status=models.CharField(max_length=200,null=True,choices=STATUS)
+    
         
