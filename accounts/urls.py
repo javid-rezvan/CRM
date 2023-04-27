e167763e68824df3dbf3f3335a00f3fc0960306d
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-04-27 20:29:33 +0200

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,7 @@ urlpatterns=[
     path('update_order/<str:pk>/',views.updateOrder,name='update_order'),
     path('delete_order/<str:pk>/',views.deleteOrder,name='delete_order'),
     path('register/',views.registerPage,name='register'),
-    path('login/',views.loginPage,name='login')
+    path('login/',views.loginPage,name='login'),
+    path('logout/',views.logoutUser,name='logout')
+
 ]
