5acb154901745a44ef9ca49242da136a73c91a93
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-04-29 17:20:05 +0430

    Update settings.py

--- crm1/settings.py
+++ crm1/settings.py
@@ -132,8 +132,8 @@ MEDIA_ROOT=BASE_DIR /'media'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST=''
-EMAIL_PORT=''
-EMAIL_USE_TLS=''
-EMAIL_HOST_USER='youremail'
-EMAIL_HOST_PASSWORD='your email password'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER='javid.rezvan@gmail.com'
+EMAIL_HOST_PASSWORD='qzfgrtvqtglbzdjt'
