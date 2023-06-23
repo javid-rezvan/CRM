adca8772c81cca01e522c7146deeb8d48f712466
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-06-23 14:12:11 +0430

    Update settings.py

--- crm1/settings.py
+++ crm1/settings.py
@@ -77,14 +77,21 @@ WSGI_APPLICATION = "crm1.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME":"DEMO_TEST",
+#         "USER":"postgres",
+#         "PASSWORD":"j-f842411",
+#         "HOST":'localhost',
+#         "PORT":'5432'
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME":"DEMO_TEST",
-        "USER":"postgres",
-        "PASSWORD":"j-f842411",
-        "HOST":'localhost',
-        "PORT":'5432'
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
